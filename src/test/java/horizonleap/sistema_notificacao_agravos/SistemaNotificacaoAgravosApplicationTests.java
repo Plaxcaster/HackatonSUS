5b30df682e496f5f30dbbd51ce490719f2c532f9
Pedro Plá <pedro.bpla@gmail.com>
2025-02-15 17:01:35 -0300

    Commit inicial do Spring Initializr

---
+++ src/test/java/horizonleap/sistema_notificacao_agravos/SistemaNotificacaoAgravosApplicationTests.java
@@ -0,0 +1,13 @@
+package horizonleap.sistema_notificacao_agravos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaNotificacaoAgravosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
